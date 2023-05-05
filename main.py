fd8fc50a3aee8ae1513440cc7e65b1100de820f6
ankit10a <ankitverma6440@gmail.com>
2023-05-05 10:18:03 +0100

    add comments and detection fram print

--- main.py
+++ main.py
@@ -15,27 +15,28 @@ data = []
 for category in categories:
     #path combine by the help of os modules
     path = os.path.join('train',category)
-
     # categories labelling
     label = categories.index(category)
 
     for file in os.listdir(path):
+        #read image of category
         image_path = os.path.join(path,file)
+        #store the image to variable
         img = cv2.imread(image_path)
         #resizing the training images
         img = cv2.resize(img,(224,224))
-
+        #append the images into list which store the image and label
         data.append([img,label])
-
 
 #check data length
 print('data',len(data))
-
+# data shuffle use is to prevent to generate the bais
 random.shuffle(data)
 
 x=[]
 y=[]
 
+# sotre the image feature and label in different list
 for features,label in data:
     x.append(features)
     y.append(label)
@@ -45,26 +46,32 @@ y = np.array(y)
 
 print("x--->",x.shape)
 print("Y",y.shape)
-
+#standard and scale the image variable
 x = x/255
 
+#training data from the model
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
 
 print("training DAta",x_train.shape)
 
 print("test data",x_test.shape)
 
+#transfer learning of the algorithm start
+
+#initialize the VGG16 Class CNN
 vgg = VGG16()
 print("vgg-->",vgg.summary())
 
+#initialize the sequential model
 model = Sequential()
 
-
+#remove the last layer of vgg 16 and add the sequentail layer
 for layer in vgg.layers[:-1]:
     model.add(layer)
 
 print("model-->",model.summary())
 
+#Freezing the layer to prevent update the weight on training
 for layer in model.layers:
     layer.trainable = False
 
@@ -73,8 +80,9 @@ model.add(Dense(1,activation="sigmoid"))
 #print model summary for the verification
 print("model-->",model.summary())
 
-#model Training an optimize configuration
+#model compilation Training an optimize configuration
 model.compile(optimizer="Adam",loss="binary_crossentropy",metrics=["accuracy"])
+
 model.fit(x_train,y_train,epochs=5,validation_data=(x_test,y_test))
 
 #to generate pickle file
--- mask_detect.py
+++ mask_detect.py
@@ -3,21 +3,11 @@ import pickle
 import keras
 import tensorflow as tf
 
-
 cap = cv2.VideoCapture(0)
 
-
-
-
 # Load the model with the specified options
-# model = tf.keras.models.load_model(model_path, options=load_options)
 def load_model():
     try:
-        # mask_pkl = open('mask.pkl','rb')
-        # print('mask_pkl',mask_pkl)
-        # loaded_model = pickle.load(mask_pkl)
-        # print('loaded_model----->', loaded_model)
-        # Set the experimental_io_device option
         load_options = tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
         model = tf.keras.models.load_model('maskModel.keras', options=load_options)
         print('model',model)
@@ -31,39 +21,40 @@ def detect_mask(img):
     y_predict = loaded_model.predict(img.reshape(1,224,224,3))
     return y_predict[0][0]
 
-
-
-# load_model = keras.models('maskModel.keras')
-
-
-# def detect_mask(img):
-#
-#     y_predi = loaded_model.predict(img.reshape(1,224,224,3))
-#
-#     return y_predi[0][0]
+def draw_label(img,text,post,bg_color):
+    text_size = cv2.getTextSize(text,cv2.FONT_HERSHEY_SIMPLEX,0.8,cv2.FILLED)
+    end_x = post[0] + text_size[0][0] +2
+    end_y = post[1] + text_size[0][1] -2
+    cv2.rectangle(img,post,(end_x,end_y),bg_color,cv2.FILLED)
+    cv2.putText(img,text,post,cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),1,cv2.LINE_AA)
 
 while True:
 
     ret, frame = cap.read()
 
-    # detection method Called
+    #resize the images
     img_resize = cv2.resize(frame, (224, 224))
-    y_pred = detect_mask(img_resize)
+    # detection method Called
+    y_pred = (detect_mask(img_resize))
 
-    print("result==>",y_pred)
-
-    # if y_pred == 0:
-    #     draw_label(frame,"Mask detection", (30,30),(0,255,0))
-    # else:
-    #     draw_label(frame,"NO Mask",(30,30),(0,0,255))
-
-    # print(y_pred)
+    print("result==>",y_pred,round(y_pred * 1000000))
+    result = round(y_pred * 1000000)
+    draw_label(frame,"Face mask Detection",(20,20),(255,0,0))
+    if result <= 99:
+        print("mask detected")
+        draw_label(frame, "Mask detection", (30, 70), (0, 255, 0))
+    else:
+        print("no mask detected")
+        draw_label(frame, "NO Mask", (30, 70), (0, 0, 255))
 
     # image show
 
     cv2.imshow('window', frame)
 
-    if cv2.waitKey(1) &cv2.waitKey(0) & 0xFF == ord('x'):
+    if cv2.waitKey(1) & 0xFF == ord('x'):
         break
 
 cv2.destroyAllWindows()
+
+
+# har = cv2.CascadeClassifier()
